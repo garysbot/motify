24381984e87652ec72dd37d3aeb921ffc5fd2e1e
Gary <g@modernhuman.xyz>
2023-11-10 14:45:28 -0500

    Frontend user auth -- CSRF and sessions

--- motify/frontend/src/store/session.js
+++ motify/frontend/src/store/session.js
@@ -16,21 +16,39 @@ const removeCurrentUser = () => {
   };
 };
 
-export const login = (user) => async (dispatch) => {
-  const { credential, password } = user;
-  const response = await csrfFetch('/api/session', {
-    method: 'POST',
-    body: JSON.stringify({
-      credential,
-      password
-    })
+const storeCSRFToken = response => {
+  const csrfToken = response.headers.get("X-CSRF-Token");
+  if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
+}
+
+const storeCurrentUser = user => {
+  if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
+  else sessionStorage.removeItem("currentUser");
+}
+
+export const login = ({ credential, password }) => async dispatch => {
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({ credential, password })
   });
   const data = await response.json();
+  storeCurrentUser(data.user);
   dispatch(setCurrentUser(data.user));
   return response;
 };
 
-const initialState = { user: null };
+export const restoreSession = () => async dispatch => {
+  const response = await csrfFetch("/api/session");
+  storeCSRFToken(response);
+  const data = await response.json();
+  storeCurrentUser(data.user);
+  dispatch(setCurrentUser(data.user));
+  return response;
+};
+
+const initialState = { 
+  user: JSON.parse(sessionStorage.getItem("currentUser"))
+};
 
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
