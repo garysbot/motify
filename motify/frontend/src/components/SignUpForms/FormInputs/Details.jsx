06261e8d0065999cb634c4f296f582d41ec92c82
garysbot <131827042+garysbot@users.noreply.github.com>
2023-11-11 20:35:32 -0500

    Styled dob fields in step 2 of user auth sign up

--- motify/frontend/src/components/SignUpForms/FormInputs/Details.jsx
+++ motify/frontend/src/components/SignUpForms/FormInputs/Details.jsx
@@ -3,60 +3,44 @@ import useFormContext from "../../../hooks/useFormContext";
 const Details = () => {
   const { data, handleChange } = useFormContext();
 
-  
-
   return (
     <>
-    <div className="signup-container">
-      <p className="helper-text">Step 2 of 3</p>
-      <p className="form-title">Tell us about yourself</p>
+      <div className="signup-container">
+        <p className="helper-text">Step 2 of 3</p>
+        <p className="form-title">Tell us about yourself</p>
 
-      <div className="form-details-field">
-        <label className="form-detail-labels">
-          Name
-        <p className="helper-text">This name will appear on your profile</p>
-          <input
-            type="text"
-            // value={data.password}
-            onChange={handleChange}
-            required
-          />
-        </label>
+        <div className="form-details-field">
+          <label className="form-detail-labels">
+            Name
+            <p className="helper-text">This name will appear on your profile</p>
+            <input
+              type="text"
+              // value={data.password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+
+        <div className="form-details-field-dob">
+          <label className="form-detail-labels">
+            Date of birth
+            <p className="helper-text">Why do we need your date of birth? <span>Learn more.</span></p>
+            <div className="dob-container"> {/* Added container for DOB fields */}
+              <select className="dropdown-width"> {/* Modified class name */}
+                <option>January</option>
+                {/* ... other month options */}
+              </select>
+              <div className="mm-yy-container"> {/* Added container for month and year fields */}
+                <input className="mm-input" placeholder="MM" /> {/* Added class name */}
+                <input className="yy-input" placeholder="YY" /> {/* Added class name */}
+              </div>
+            </div>
+          </label>
+        </div>
       </div>
-
-      <div className="form-details-field">
-        <label className="form-detail-labels">
-          Date of birth
-        <p className="helper-text">Why do we need your date of birth? <span>Learn more.</span></p>
-          <select>
-            <option>January</option>
-            <option>February</option>
-            <option>March</option>
-            <option>April</option>
-            <option>May</option>
-            <option>June</option>
-            <option>July</option>
-            <option>August</option>
-            <option>September</option>
-            <option>October</option>
-            <option>November</option>
-            <option>December</option>
-          </select>
-          <select>
-            <option>Hello</option>
-          </select>
-        </label>
-      </div>
-
-      
-
-
-
-
-    </div>
-  </>
+    </>
   );
-
 };
 
 export default Details;
