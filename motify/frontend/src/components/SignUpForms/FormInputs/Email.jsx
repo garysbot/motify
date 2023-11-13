978173522cb61b971450578fdd1e503a95ef5c4e
Gary <g@modernhuman.xyz>
2023-11-13 15:22:56 -0500

    Finish sign up form front end validation

--- motify/frontend/src/components/SignUpForms/FormInputs/Email.jsx
+++ motify/frontend/src/components/SignUpForms/FormInputs/Email.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import useFormContext from "../../../hooks/useFormContext";
 import './FormInputs.css'
 
@@ -7,39 +6,32 @@ const Email = () => {
 
   return (
     <>
-    {/* {console.log(`Email page opened`)} */}
-
-    <div className="signup-container">
-
-      <h1>Sign up to start listening</h1>
-
-      <div className="form-field-container">
-
-        <label className="form-field-full-width">
-          Email address
-          <input
-            type="text"
-            name="email"
-            value={data.email}
-            onChange={handleChange}
-            placeholder='name@domain.com'
-            required
-            className="text-input"
-            />
-        </label>
-        {/* //! Error Handling for Email */}
-        { emailError &&  
+      <div className="signup-container">
+        <h1>Sign up to start listening</h1>
+        <div className="form-field-container">
+          <label className="form-field-full-width">
+            Email address
+            <input
+              className="text-input"
+              type="text"
+              name="email"
+              placeholder='name@domain.com'
+              value={data.email}
+              onChange={handleChange}
+              required
+              />
+          </label>
+          {/* Email Error Handling */}
+          { 
+            emailError &&  
             <div className='field-error-container'>
               <div className='field-error-icon'></div>
               <p className='field-error-text'>{emailError}</p>
             </div>
-        }
-
+          }
+        </div>
       </div>
-
-    </div>
-
-  </>
+    </>
   );
 
 };
--- motify/frontend/src/context/FormContext.jsx
+++ motify/frontend/src/context/FormContext.jsx
@@ -1,19 +1,22 @@
 import { createContext, useState, useEffect } from "react";
 
+// Create the FormContext
 const FormContext = createContext({});
 
+// Create the FormProvider to provide the context to other components
 export const FormProvider = ({ children }) => {
+  // Page State -- determine which sign up page we're on
+  const [page, setPage] = useState(0);
 
-  // ! Control which form component to display
+  //  Control which signup form component to display
   const formPage = {
     0: 'email',
     1: 'password',
     2: 'details',
     3: 'marketing'
-  }
+  };
 
-  const [page, setPage] = useState(0)
-
+  // Data State -- saves each signup field via handleChange
   const [data, setData] = useState({
     email: '',
     password: '',
@@ -21,55 +24,153 @@ export const FormProvider = ({ children }) => {
     birth_date: '',
     gender: '',
     optinmarketing: false
-  })
+  });
 
-  // ! handleChange for form components
-  // checks React tag attrib "type" and "name" to deploy correct data setter
-  // const handleChange = e => {
-  //   const type = e.target.type
+  // Error States
+  const [emailError, setEmailError] = useState();
+  const [passwordError, setPasswordError] = useState();
+  const [usernameError, setUsernameError] = useState();
+  const [monthError, setMonthError] = useState();
+  const [dayError, setDayError] = useState();
+  const [yearError, setYearError] = useState();
+  const [genderError, setGenderError] = useState();
 
-  //   const name = e.target.name
-
-  //   const value = type === "checkbox"
-  //       ? e.target.checked
-  //       : e.target.value
-
-  //   setData(prevData => ({
-  //       ...prevData,
-  //       [name]: value
-  //   }))
-  // }
-
-  const [emailError, setEmailError] = useState()
+  // Field Validations
+  // Helper for Month and Year validation
+  function isInteger(str) {
+    // Define a regular expression pattern for integers (positive or negative)
+    const integerPattern = /^-?\d+$/;
   
-  // ! Updated handleChange with email validation
-  const handleChange = e => {
-    const type = e.target.type;
-    const name = e.target.name;
-    const value = type === "checkbox" ? e.target.checked : e.target.value;
-  
-    // Email validation function
-    const validateEmail = (email) => {
-      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  
-      if (!emailRegex.test(email)) {
-        setEmailError(`This email is invalid. Make sure it's written like example@email.com`);
-        return false;
-      }
-      setEmailError('');
-        return true;
-      };
-  
-      // Check if the changed field is 'email' and validate it
-      if (name === 'email') {
-        validateEmail(value);
-      }
-    
-      setData(prevData => ({
-        ...prevData,
-        [name]: value
-      }));
+    // Use the test() method to check if the string matches the pattern
+    return integerPattern.test(str);
   }
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (!emailRegex.test(email) && email.length >= 5) {
+      setEmailError(`This email is invalid. Make sure it's written like example@email.com`);
+      return false;
+    }
+    setEmailError('');
+      return true;
+  };
+
+  const validatePassword = (password) => {
+    let passwordChars = password.split("")
+    const SPECIAL_CHARS = [
+      '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '|', `/`
+    ]
+
+    if(password.length <= 3){
+      setPasswordError(`Password should contain at least 8 characters.`);
+      return false;
+    } else if (password.length >= 255) {
+      setPasswordError(`Password should contain less than 255 characters.`);
+    } else if (!SPECIAL_CHARS.some(char => passwordChars.includes(char))) {
+      setPasswordError(`Password should include at least one special character.`);
+      return false;
+    } else {
+      setPasswordError('');
+        return true;
+    }
+  };
+
+  const validateUsername = (username) => {
+    if (username.length < 3){
+      setUsernameError(`Name should contain at least 3 characters.`);
+      return false
+    } else {
+      setUsernameError('');
+      return true;
+    }
+  };
+
+  const validateMonth = (month) => {
+    const MONTHS = [
+      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    ]
+    if (MONTHS.includes(month)) {
+      setMonthError('');
+      return true
+    } else {
+      setMonthError('Select a month.')
+      return false;
+    }
+  }
+
+  const validateDay = (day) => {
+    if (!isInteger(day)){
+      setDayError('Must be a valid day');
+      return false;
+    }
+
+    if (day < 1 || day > 31) {
+      setDayError('Must be a valid day.')
+      return false;
+    } else {
+      setDayError('');
+      return true;
+    }
+  }
+
+  const validateYear = (year) => {
+    if (!isInteger(year)){
+      setYearError('Must be a valid year.');
+      return false
+    }
+
+    if (year.length === 4 && (year < 1900 || year > 2023)) {
+      setYearError('Must be a valid year.')
+      return false;
+    } else {
+      setYearError('');
+      return true;
+    }
+  }
+  
+  // Handle Change -> real-time error validation
+  const handleChange = e => {
+    // Define field type
+    const type = e.target.type;
+    // Define field name -- i.e. email / password / name birthdate gender / marketingoptin
+    const name = e.target.name;
+    // Define value -- ternary handler for marketingOptIn checkbox
+    const value = type === "checkbox" ? e.target.checked : e.target.value;
+    
+    // Validation Switch Case
+    switch (name) {
+      case 'email':
+        validateEmail(value)
+        break;
+      case 'password':
+        validatePassword(value)
+        break;
+      case 'username':
+        validateUsername(value)
+        break;
+      case 'month':
+        console.log(`switch case worked for month`)
+        validateMonth(value)
+        break;
+      case 'day':
+        console.log(`switch case worked for day`)
+        validateDay(value)
+        break;
+      case 'year':
+        console.log(`switch case worked for year`)
+        validateYear(value)
+        break;
+      default:
+        break;
+    }
+    
+    // Set the data upon completed front-end validations
+    setData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
   
 
   // ^ Validating all required fields are filled out before allowing submit?
@@ -134,7 +235,7 @@ export const FormProvider = ({ children }) => {
   const submitHide = page !== Object.keys(formPage).length - 1 && "remove-button"
 
   return (
-    <FormContext.Provider value={{ formPage, page, setPage, data, setData, canSubmit, handleChange, disableNext, prevHide, nextHide, submitHide, handlePrev, handleNext, emailError, setEmailError }}>
+    <FormContext.Provider value={{ formPage, page, setPage, data, setData, canSubmit, handleChange, disableNext, prevHide, nextHide, submitHide, handlePrev, handleNext, emailError, setEmailError, passwordError, setPasswordError, usernameError, setUsernameError, monthError, setMonthError, dayError, setDayError, yearError, setYearError }}>
         {children}
     </FormContext.Provider>
 )
