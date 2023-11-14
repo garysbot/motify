97117e902b941e61aa4cc6cc2d4ce6c4d0906936
Gary <g@modernhuman.xyz>
2023-11-14 15:27:57 -0500

    Added ProfileIcon and DropdownMenu uireducer, redux ui state, closes when user clicks out of profile menu. Added log out button to this dropdown

---
+++ motify/frontend/src/components/UserHomePage/DropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,15 @@
+import '../UserHomePage.css'
+
+import React from 'react';
+
+const DropdownMenu = () => {
+  return (
+    <div className="userbar-profile-menu">
+      <ul>
+        <li>Log out</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- motify/frontend/src/store/index.js
+++ motify/frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
+import uiReducer from './uireducer';
 
 export const rootReducer = combineReducers({
-  session
+  session,
+  ui: uiReducer
 });
 
 let enhancer;
