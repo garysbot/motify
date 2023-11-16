6e3e2cf600fe91dd67809c01081bd25c7eb4d7c6
Gary <g@modernhuman.xyz>
2023-11-16 14:50:00 -0500

    Update Album show jbuilder to display song and artist data too

--- motify/frontend/src/components/ShowPage/ShowPage.jsx
+++ motify/frontend/src/components/ShowPage/ShowPage.jsx
@@ -1,8 +1,9 @@
 import './ShowPage.css'
 import { useAlbum } from './ShowHooks/useAlbum';
+import kendrickAlbumCover from '../../static/albums/covers/kendrick.png'
 
 const ShowPage = () => {
-  const album = useAlbum(45)
+  const album = useAlbum(129)
 
   if (!album) {
     return <div>Loading...</div>;
@@ -12,30 +13,55 @@ const ShowPage = () => {
   return (
     <>
       <div className='show-banner'>
-        {/* Image Here */}
-        <p>Album</p>
-        <h1>Mr. Morale & The Big Steppers</h1>
-        <p>Kendrick Lamar - 2022 - 19 songs, 1hr 18 mins</p>
-
         {/* Add BG to this Div? */}
+        <img src={kendrickAlbumCover} className='album-cover-img'></img>
         <div className='banner-details'>
-          {/* Album/Playlist/Song Image */}
-          {/* Resource Type */}
-          {/* Album Title */}
+          <p>Album</p>
+          <h1>{album.title}</h1>
+          <p>{album.artistName} {album.releaseDate}</p>
+          {
+            album.songs.map((song)=>
+              <>
+              <p>hi?</p>
+              <p>Title:{song.title}</p>
+              </>
+            )
+          }
+
+          <p>Kendrick Lamar - 2022 - 19 songs, 1hr 18 mins</p>
           {/* Artist Name */}
           {/* Release Year */}
           {/* Num Songs */}
           {/* Total Length */}
         </div>
-        <div className='show-play-bar'>
-          {/* Play Button */}
-          {/* Like || Follow Button */}
-          {/* Menu Dots */}
+      </div>
+      <div className='show-play-bar'>
+        {/* Play Button */}
+        {/* Like || Follow Button */}
+        {/* Menu Dots */}
+      </div>
+      <div className='show-content'>
+        <div className='show-songs-header'>
+          {/* Album/Playlist Table Header */}
+          <p>#</p>
+          <p>Title</p>
+          {/* Time Button */}
         </div>
-        <div className='show-content'>
-          {/* Album Table Header */}
-          {/* Playlist Table Header */}
-          {/* Artist Popular Table */}
+        <div className='show-songs-table'>
+          <div className='show-songs-row'>
+            {/* Track Num */}
+            <p>1</p>
+            {/* Song Title & Artist Name */}
+            <div className='song-title-artist'>
+              <p>United in Grief</p>
+              {/* explicit or not */}
+              <p>Kendrick Lamar</p>
+            </div>
+            {/* Liked/NotLiked */}
+            {/* Duration */}
+            <p>4:15</p>
+          </div>
+
         </div>
       </div>
     </>
