66bc76f527fc2ef7ec9ca32b1498f5a18ce05d72
garysbot <131827042+garysbot@users.noreply.github.com>
2023-11-18 11:13:05 -0500

    Finish styling play/pause button in PlayBar

--- motify/frontend/src/components/PlayBar/PlayBarContent.jsx
+++ motify/frontend/src/components/PlayBar/PlayBarContent.jsx
@@ -1,9 +1,14 @@
 import kendrickAlbumCover from '../../static/albums/covers/kendrick.png'
-
+import { ReactComponent as PlayButton } from '../../static/playbar/show/show-play-bar-play-button.svg'
+import { ReactComponent as PlayButtonCircle } from '../../static/playbar/show/show-play-bar-play-circle.svg'
+import { ReactComponent as PauseButton } from '../../static/playbar/show/show-play-bar-pause-button.svg'
 
 const PlayBarContent = ({ contentType }) => {
   const content = {
-    kendrickAlbumCover
+    kendrickAlbumCover,
+    PlayButton,
+    PlayButtonCircle,
+    PauseButton
   }
 
   const ContentComponent = content[contentType]
