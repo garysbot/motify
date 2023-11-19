1b619639a37fc87f766470933f71ac4168f1a012
Gary <g@modernhuman.xyz>
2023-11-19 10:55:07 -0500

    Functionality play pause playbar

--- motify/frontend/src/components/PlayBar/PlayBar.jsx
+++ motify/frontend/src/components/PlayBar/PlayBar.jsx
@@ -1,51 +1,8 @@
 import './PlayBar.css'
 import kendrickAlbumCover from '../../static/albums/covers/kendrick.png'
 import PlayBarContent from './PlayBarContent';
-import React, { useRef, useState, useEffect } from 'react';
-import { ReactComponent as PlayButton } from '../../static/playbar/show/show-play-bar-play-button.svg'
-import { ReactComponent as PauseButton } from '../../static/playbar/show/show-play-bar-pause-button.svg'
-import { ReactSVG } from 'react-svg';
-
 
 const PlayBar = ({ currentSong }) => {
-
-  const audioRef = useRef(new Audio());
-  const [isPlaying, setIsPlaying] = useState(true);
-
-  useEffect(() => {
-    if (currentSong) {
-      audioRef.current.src = currentSong;
-    }
-  }, [currentSong])
-
-  useEffect(() => {
-    audioRef.current.onplay = () => setIsPlaying(true);
-    audioRef.current.onpause = () => setIsPlaying(false);
-    audioRef.current.onerror = () => {
-        // Handle audio playback errors
-    };
-  }, []);
-
-  const playAudio = () => {
-    if (currentSong) {
-      console.log(`Play Clicked`)
-      audioRef.current.play();
-    }
-  };
-
-  const pauseAudio = () => {
-    console.log(`Pause Clicked`)
-    audioRef.current.pause();
-  };
-
-  const handleClick = () => {
-    if (isPlaying) {
-      pauseAudio();
-    } else {
-      playAudio();
-    }
-  }
-
 
   return (
     <>
@@ -65,17 +22,10 @@ const PlayBar = ({ currentSong }) => {
           <div className='controls-container'>
             <PlayBarContent contentType='ShuffleButton'/>
             <PlayBarContent contentType='PrevButton'/>
-            {/* <p>Previous</p> */}
+
             <div className='play-pause-button'>
               <div className='play-button-content'>
-                <button onClick={handleClick}>
-                  {
-                    isPlaying ? 
-                      <PlayBarContent contentType="PauseButton"/>
-                      :
-                      <PlayBarContent contentType='PlayButton'/>
-                  }
-                </button>
+                <PlayBarContent contentType='PlayButton' currentSong={currentSong} className='play-pause-button'/>
               </div>
               <div className='play-button-circle-content'>
                 <PlayBarContent contentType='PlayButtonCircle'/>
