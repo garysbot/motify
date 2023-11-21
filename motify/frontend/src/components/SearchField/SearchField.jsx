765029d7c66fb46fb2c9b224cf6d00690903eb20
Gary <g@modernhuman.xyz>
2023-11-21 15:01:19 -0500

    Working Redux state updates for start_search, update_query, and Update_search_results

--- motify/frontend/src/components/SearchField/SearchField.jsx
+++ motify/frontend/src/components/SearchField/SearchField.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import searchIcon from '../../static/icons/search-inactive.svg'
-import closeIcon from '../../static/icons/close.svg'
+import { useSelector, useDispatch } from 'react-redux';
+import searchIcon from '../../static/icons/search-inactive.svg';
+import closeIcon from '../../static/icons/close.svg';
 import { performSearch, clearSearch } from '../../store/search';
 
 const SearchField = () => {
   const [inputValue, setInputValue] = useState('');
   const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.search.isLoading);
+  const error = useSelector(state => state.search.error);
 
   const handleChange = (event) => {
     const newQuery = event.target.value;
@@ -17,7 +19,6 @@ const SearchField = () => {
       dispatch(clearSearch()); // Clearing the search when input is empty
     }
   };
-  
 
   const clearInput = () => {
     setInputValue('');
@@ -36,6 +37,8 @@ const SearchField = () => {
         />
         <img src={closeIcon} alt='Close' className='close-icon' onClick={clearInput}/>
       </div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
     </>
   );
 }
--- motify/frontend/src/store/search.js
+++ motify/frontend/src/store/search.js
@@ -1,18 +1,31 @@
 import csrfFetch from "./csrf";
+import { debounce } from 'lodash';
 
 // ! Actions
 export const START_SEARCH = 'search/START_SEARCH';
+export const UPDATE_QUERY = 'search/UPDATE_QUERY';
 export const UPDATE_SEARCH_RESULTS = 'search/UPDATE_SEARCH_RESULTS';
+export const SEARCH_ERROR = 'search/SEARCH_ERROR';
 export const CLEAR_SEARCH = 'search/CLEAR_SEARCH';
 
 export const startSearch = () => ({
   type: START_SEARCH,
 })
 
+export const updateQuery = (query) => ({
+  type: UPDATE_QUERY,
+  payload: query
+});
+
 export const updateSearchResult = (searchResult) => ({
   type: UPDATE_SEARCH_RESULTS,
   payload: searchResult
 })
+
+export const searchError = (error) => ({
+  type: SEARCH_ERROR,
+  payload: error
+});
 
 export const clearSearch = () => ({
   type: CLEAR_SEARCH
@@ -21,27 +34,37 @@ export const clearSearch = () => ({
 const initialState = {
   query: '',
   results: [],
-  isLoading: false
+  isLoading: false,
+  error: null
 };
 
-export const performSearch = (query) => async (dispatch) => {
-  dispatch(startSearch());
+// Debounce function
+const debounceSearch = debounce(async (query, dispatch) => {
   try {
     const response = await csrfFetch(`/api/search?q=${query}`);
     const data = await response.json();
     dispatch(updateSearchResult(data));
   } catch (error) {
-    // dispatch an error action or handle the error
+    dispatch(searchError(error));
   }
-};
+}, 500); // 500 ms delay
 
+export const performSearch = (query) => (dispatch) => {
+  dispatch(updateQuery(query)); // Update the query state
+  dispatch(startSearch());
+  debounceSearch(query, dispatch); // Debounced search
+};
 
 const searchReducer = (state = initialState, action) => {
   switch (action.type) {
-    case START_SEARCH: 
-      return { ...state, isLoading: true }
+    case START_SEARCH:
+      return { ...state, isLoading: true, error: null }
+    case UPDATE_QUERY:
+      return { ...state, query: action.payload };
     case UPDATE_SEARCH_RESULTS:
       return { ...state, results: action.payload, isLoading: false }
+    case SEARCH_ERROR:
+      return { ...state, error: action.payload, isLoading: false }
     case CLEAR_SEARCH:
       return initialState;
     default:
@@ -49,4 +72,4 @@ const searchReducer = (state = initialState, action) => {
   }
 }
 
-export default searchReducer
+export default searchReducer;
