cbfa33f9758c2e4e2b4ac7e8527500a62e20959e
garysbot <131827042+garysbot@users.noreply.github.com>
2023-12-26 15:09:38 -0500

    Styling for rendered vertical cards on ShowArtistPage

--- frontend/src/components/ShowPages/ShowArtistPage.jsx
+++ frontend/src/components/ShowPages/ShowArtistPage.jsx
@@ -11,8 +11,6 @@ const ShowArtistPage = () => {
   const dispatch = useDispatch()
   const artist = useSelector((state) => state.artists[artistId])
   const albums = Object.values(useSelector((state) => state.albums))
-  const artistAlbums = albums.filter((album) => album.artistName === artist.artistName)
-  
   
   useEffect(() => {
     const fetchArtistData = async () => {
@@ -21,7 +19,6 @@ const ShowArtistPage = () => {
         dispatch(fetchArtist(artistId))
         setIsLoading(false)
       }
-      
     }
     fetchArtistData()
   }, [dispatch, artistId])
@@ -30,14 +27,13 @@ const ShowArtistPage = () => {
     return albums
       .filter((album) => album.artistName === artist.artistName)
       .map((album) => (
-        <div className='content-cards' key={album.id}> {/* Ensure each child in a list has a unique key prop */}
-          <div className='content-cards-container'>
-            <div className='vertical-content-card'>
-              <p className='vertical-title'><Link to={`/albums/${album.id}`}>{album.title}</Link></p>
-              <p className='vertical-artist'>{artist.artistName}</p>
-            </div>
+        <Link to={`/albums/${album.id}`}>
+          <div className='vertical-content-card'  key={album.id}>
+            <img src={album.coverImg} alt='' className='vertical-cover'></img>
+            <p className='vertical-title'>{album.title}</p>
+            <p className='vertical-artist'>{artist.artistName}</p>
           </div>
-        </div>
+        </Link>
       ));
   }
 
@@ -45,33 +41,22 @@ const ShowArtistPage = () => {
     return <div>Loading...</div>;
   }
 
-  
-
-  // if (albumsByArtist.length === 0) {
-  //   return <div>No albums found for this artist</div>
-  // }
-
-  // ! debugging
-  const handleClick = () => {
-    console.log(`hello world? ${artistId}`)
-  }
-
-  // ! ----------------------------------
-
   return (
     <>
       <div 
         className='show-banner'
-        style={{'background-color':'red'}}
+        // style={{'background-color':'red'}}
       >
         <div className='banner-details banner-artist-name'>
           <p>Artist</p>
-          <h1 onClick={handleClick}>{artist?.artistName}</h1>
+          <h1>{artist?.artistName}</h1>
         </div>
       </div>
-
-      <ArtistAlbums/>
-
+      <div className='content-cards'>
+        <div className='content-cards-container'>
+          <ArtistAlbums/>
+        </div>
+      </div>
     </>
   );
 }
