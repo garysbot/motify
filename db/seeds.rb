ba3ced6b52c5b63989ad31061cc639fa472d45cf
garysbot <131827042+garysbot@users.noreply.github.com>
2024-01-03 17:03:56 -0500

    add new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -322,67 +322,67 @@ ApplicationRecord.transaction do
   #  ! -------------------------------------------------------------------------------------------
 
   # ^ The Rest of the Database (Faker)
-  10.times do 
-    # User.create!({
-    #   username: Faker::Internet.unique.username(specifier: 3),
-    #   email: Faker::Internet.unique.email,
-    #   password: 'password',
-    #   birth_date: DateTime.new(rand(1970..2005), rand(1..12), rand(1..28)),
-    #   gender: ['Male', 'Female', 'Other'].sample,
-    #   optinmarketing: [true, false].sample
-    # }) 
+  # 10.times do 
+  #   # User.create!({
+  #   #   username: Faker::Internet.unique.username(specifier: 3),
+  #   #   email: Faker::Internet.unique.email,
+  #   #   password: 'password',
+  #   #   birth_date: DateTime.new(rand(1970..2005), rand(1..12), rand(1..28)),
+  #   #   gender: ['Male', 'Female', 'Other'].sample,
+  #   #   optinmarketing: [true, false].sample
+  #   # }) 
 
-    # Function to create mock artists
-    def create_mock_artists(number_of_artists)
-      number_of_artists.times do
-        Artist.create(
-          artist_name: Faker::Music.band,
-          verified: [true, false].sample,
-          about_blurb: Faker::Lorem.sentence(word_count: 20),
-          about_img: Faker::LoremFlickr.image(size: "300x300", search_terms: ['artist']),
-          global_ranking: rand(1..1000),
-          monthly_listeners: rand(10000..1000000)
-        )
-      end
-    end
+  #   # Function to create mock artists
+  #   def create_mock_artists(number_of_artists)
+  #     number_of_artists.times do
+  #       Artist.create(
+  #         artist_name: Faker::Music.band,
+  #         verified: [true, false].sample,
+  #         about_blurb: Faker::Lorem.sentence(word_count: 20),
+  #         about_img: Faker::LoremFlickr.image(size: "300x300", search_terms: ['artist']),
+  #         global_ranking: rand(1..1000),
+  #         monthly_listeners: rand(10000..1000000)
+  #       )
+  #     end
+  #   end
 
-    # Function to create mock albums
-    def create_mock_albums(number_of_albums)
-      artist_ids = Artist.where.not(id: 1).pluck(:id)
+  #   # Function to create mock albums
+  #   def create_mock_albums(number_of_albums)
+  #     artist_ids = Artist.where.not(id: 1).pluck(:id)
 
-      number_of_albums.times do
-        Album.create(
-          artist_id: artist_ids.sample,
-          title: Faker::Music.album,
-          genre: Faker::Music.genre,
-          cover_img: Faker::LoremFlickr.image(size: "300x300", search_terms: ['album']),
-          release_date: rand(1900..2023),
-          record_company: Faker::Company.name
-        )
-      end
-    end
+  #     number_of_albums.times do
+  #       Album.create(
+  #         artist_id: artist_ids.sample,
+  #         title: Faker::Music.album,
+  #         genre: Faker::Music.genre,
+  #         cover_img: Faker::LoremFlickr.image(size: "300x300", search_terms: ['album']),
+  #         release_date: rand(1900..2023),
+  #         record_company: Faker::Company.name
+  #       )
+  #     end
+  #   end
 
-    # Function to create mock songs
-    def create_mock_songs(number_of_songs)
-      album_ids = Album.where.not(artist_id: 1).pluck(:id)
+  #   # Function to create mock songs
+  #   def create_mock_songs(number_of_songs)
+  #     album_ids = Album.where.not(artist_id: 1).pluck(:id)
     
-      number_of_songs.times do
-        Song.create(
-          artist: Artist.where.not(id: 1).sample,  # Use artist object
-          album_id: album_ids.sample, # Use album ID
-          title: Faker::Music::RockBand.song,
-          duration: rand(120..300),
-          explicit: [true, false].sample
-        )
-      end
-    end
+  #     number_of_songs.times do
+  #       Song.create(
+  #         artist: Artist.where.not(id: 1).sample,  # Use artist object
+  #         album_id: album_ids.sample, # Use album ID
+  #         title: Faker::Music::RockBand.song,
+  #         duration: rand(120..300),
+  #         explicit: [true, false].sample
+  #       )
+  #     end
+  #   end
 
     
-    # Create artists, albums, and songs
-    create_mock_artists(10)
-    create_mock_albums(50)
-    create_mock_songs(200)
-  end
+  #   # Create artists, albums, and songs
+  #   create_mock_artists(10)
+  #   create_mock_albums(50)
+  #   create_mock_songs(200)
+  # end
 
   puts "Done!"
 end
